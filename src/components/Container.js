cce730a835324f831fd47d64fd566775b3259296
Jesse Ward <=>
2017-06-26 22:05:56 -0400

    reformatted Container to bind() pattern and added 'Enter' key functionality

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,6 +5,13 @@ import List from './List';
  * The container for the whole app
  */
 class Container extends React.Component {
+  constructor() {
+    super();
+    
+
+    this.addItemHandler = this.addItemHandler.bind(this);
+    this.shouldSubmit = this.shouldSubmit.bind(this);
+  }
   addItemHandler() {
     if (this.itemNameInput.value && this.itemNameInput.value.trim()) {
       this.list.addItem(this.itemNameInput.value.trim());
@@ -13,11 +20,17 @@ class Container extends React.Component {
     this.itemNameInput.value = "";
   }
 
+  shouldSubmit(e) {
+    if (e.charCode === 13) {
+      this.addButton.click();
+    }
+  }
+
   render() {
     return (
       <div id="container">
-        Item Name: <input ref={(inputEl) => { this.itemNameInput = inputEl }} type="text" />
-        <button onClick={(e) => this.addItemHandler()}>Add Item</button>
+        Item Name: <input ref={(inputEl) => { this.itemNameInput = inputEl }} type="text" onKeyPress={this.shouldSubmit} />
+        <button ref={(addButton) => this.addButton = addButton} onClick={this.addItemHandler}>Add Item</button>
         <List ref={(listObj) => { this.list = listObj }} />
       </div>
     );
